e46c61bb0b9e4557b4bb283f30d1004a96922376
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2021-07-10 22:06:06 +0200

    Initial Commit

---
+++ Template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using ll = long long ;
+using ull = unsigned long long ;
+#define pint  pair<int,int>
+#define vint  vector <int> 
+#define vpint  vector <pair<int,int>> 
+
+#define Hello_World ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+#define letMeIn freopen ("in.txt","r",stdin); freopen ("out.txt","w",stdout);
+#define USACO(x) freopen (x".in","r",stdin); freopen (x".out","w",stdout);
+#define sortv(v) sort(v.begin(),v.end())
+#define revv(v) reverse(v.begin(),v.end())
+#define rep(x) for(int xzcv5423 = 0 ; xzcv5423 < x ; xzcv5423++)
+
+#define yup puts("YES");
+#define nope puts("NO");
+#define F first
+#define S second
+#define pb push_back
+#define pi 3.14159265359
+#define endl "\n"
+using namespace std;
+
+void takeintv(vector <int>& v,int size){
+    for(int i = 0; i < size ; i++)
+        cin>> v[i];
+}
+
+void printv(vector <int>& v){
+    for(int i = 0; i < v.size() ; i++)
+        cout << v[i] << " ";
+}
