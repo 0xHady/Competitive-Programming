02d7284798f5185e8e67f2258cb9c3931fc08354
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2021-09-13 15:43:04 +0200

    Update prime_Factorization.cpp

--- Library/prime_Factorization.cpp
+++ Library/prime_Factorization.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define pb push_back
+#define Time cerr << "\nTime Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
 vector <int> res;
-
 
 void primeFact(int n){
     for(int i = 2 ; i*i <= n ;i++)
@@ -18,4 +18,6 @@ int main(){
         int n; cin >>n ;
         primeFact(n);
         for(auto &x:res) cout << x << ' ';
+    
+    Time
 }
