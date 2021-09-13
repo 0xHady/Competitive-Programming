1340b46b901f28baa8f1b5ed3a412a9a67a56ab4
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2021-09-13 15:41:07 +0200

    Add files via upload

---
+++ Library/Divisors.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define Time cerr << "\nTime Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+
+vector <int> divs1; // sorted in non-decrecing order 1 2 3
+vector <int> divs2; // sorted in non-ascending order 9 8 4
+
+void fillDivs(int n){
+    for(int i = 1 ; i*i <= n ;i++){
+        if(!(n%i)){
+            divs1.pb(i);
+            if(i != (n/i))
+                divs2.pb(n/i);
+        }
+    }
+}
+
+// you can export the list into one vector
+// and sort it if you can 
+// sort takes O(nlog(n)) 
+// so, it depends on the constraints
+
+int main(){
+
+    int n ; cin >> n;
+    fillDivs(n);
+    for(auto &x:divs2) cout << x << ' ';
+    for(int i = divs1.size()-1 ; i >= 0 ; i--) cout << divs1[i] << ' ';
+
+    Time
+}
+
+/*
+ *
+ * A number is divisible by 2  if the last digit is 0, 2, 4, 6 or 8.
+ * A number is divisible by 3  if the sum of the digits is divisible by 3.	
+ * A number is divisible by 4  if the number formed by the last two digits is divisible by 4.	
+ * A number is divisible by 5  if the last digit is either 0 or 5.	
+ * A number is divisible by 6  if it is divisible by 2 AND it is divisible by 3.	
+ * A number is divisible by 8  if the number formed by the last three digits is divisible by 8.	
+ * A number is divisible by 9  if the sum of the digits is divisible by 9.	
+ * A number is divisible by 10  if the last digit is 0.	
+ * 
+ */
