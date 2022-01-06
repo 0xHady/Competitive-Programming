ee5e92d66a63f0a1ce1c779a008811ad1ed99b9c
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2022-01-06 16:58:27 +0200

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -19,15 +19,14 @@ typedef unsigned long long int ull;
 #define rall(n) n.rbegin(),n.rend()
 #define sz(xxxf) (int)(xxxf.size())
 #define log4j(hkjl) cout << hkjl << endl
+#define read(FILE) freopen(FILE,"r",stdin);
 #define mem(a, x) memset(a , x , sizeof(a))
 #define tk(x) for(auto &zeko:x) cin >> zeko;
 #define Num_of_Digits(n) ((int)log10(n) + 1)
-ll myceil(ll a,ll b){return ((a/b)+((a%b)!=0));}
+#define write(FILE) freopen(FILE,"w",stdout);
 #define debug(x) cerr << #x << ": " << (x) << endl;
-pair<int,int> DIREC[4] = {{0,-1},{0,1},{1,0},{-1,0}};
 #define print_vec(v) for(auto &zkksd:v) cout << zkksd << ' ' ;
-#define lzeros(mask) __builtin_clz(mask) // no of leading zeros
-#define tzeros(mask) __builtin_ctz(mask) // no of trailing zeros
+template <class T> T myceil(T a,T b){return ((a/b)+((a%b)!=0));}
 template <class T> bool isSquare (T n) {T sq = sqrt(n);  return (sq*sq)==n;}
 template <class T, class U> T GCD (T a, U b) {return (!b ? a : GCD(b, a%b));}
 template <class T, class U> T LCM (T a, U b) {return ((a/GCD(a, b)) * b);}
@@ -37,13 +36,19 @@ string lowercase(string &s) {transform(s.begin(), s.end(), s.begin(), ::tolower)
 #define thats_what_she_said int main(){ ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
 template <class T> void MAKE_UNIQUE(T &x){sort(all(x));x.resize(distance(x.begin(),unique(all(x))));}
 template <class T> T ecDst(T x1, T x2, T y1, T y2){ return pow((double)x2-x1,2.0)+(pow((double)y2-y1,2.0)); }
-//freopen("output.txt","w",stdout); 
-thats_what_she_said // x minutes 
-    //freopen("jumping.in","r",stdin); 
+
+/*
+ *
+ * x minutes
+ *
+ */
+
+/**/thats_what_she_said/**/
     int T = 1;
     //cin >> T;
     while(T--)
     {
+
     }
     Time
 }
