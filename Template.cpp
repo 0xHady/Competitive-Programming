4709ed64baad271dd82a45e83481e17155c3ed45
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2022-01-06 16:49:07 +0200

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -5,36 +5,38 @@ using namespace std;
 #define endl "\n"
 #define pb push_back
 typedef long long ll;
-typedef vector<int> vi;
+typedef vector<int> vint;
+void donothing(){return;}
+#define PQ priority_queue
+const ll MOD = 1000000007;
 typedef pair<int,int> pint;
-const int mod = 1e9 + 7;
+typedef vector<pint> vpint;
 const double PI = acos(-1);
-#define sz(xxxf) (int)(xxxf.size())
 #define all(n) n.begin(),n.end()
 #define nope cout << "NO" << endl;
 #define yup cout << "YES" << endl;
 typedef unsigned long long int ull;
 #define rall(n) n.rbegin(),n.rend()
-#define mem(a, x) memset(a , x , sizeof(a))
-#define Num_of_Digits(n) ((int)log10(n) + 1)
-#define debug(x) cerr << #x << ": " << (x) << endl;
-#define lzeros(mask) __builtin_clz(mask) // no of leading zeros
-#define print_vec(v) for(auto &zkksd:v) cout << zkksd << ' ' ;
-#define tzeros(mask) __builtin_ctz(mask) // no of trailing zeros
-#define Time cerr << "\nTime Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
-#define thats_what_she_said int main(){ ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
-#define tk(x) for(auto &zeko:x) cin >> zeko;
+#define sz(xxxf) (int)(xxxf.size())
 #define log4j(hkjl) cout << hkjl << endl
-string uppercase(string &s) {transform(s.begin(), s.end(), s.begin(), ::toupper); return s;}
-string lowercase(string &s) {transform(s.begin(), s.end(), s.begin(), ::tolower); return s;}
+#define mem(a, x) memset(a , x , sizeof(a))
+#define tk(x) for(auto &zeko:x) cin >> zeko;
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+ll myceil(ll a,ll b){return ((a/b)+((a%b)!=0));}
+#define debug(x) cerr << #x << ": " << (x) << endl;
+pair<int,int> DIREC[4] = {{0,-1},{0,1},{1,0},{-1,0}};
+#define print_vec(v) for(auto &zkksd:v) cout << zkksd << ' ' ;
+#define lzeros(mask) __builtin_clz(mask) // no of leading zeros
+#define tzeros(mask) __builtin_ctz(mask) // no of trailing zeros
+template <class T> bool isSquare (T n) {T sq = sqrt(n);  return (sq*sq)==n;}
 template <class T, class U> T GCD (T a, U b) {return (!b ? a : GCD(b, a%b));}
 template <class T, class U> T LCM (T a, U b) {return ((a/GCD(a, b)) * b);}
-template <class T> bool isSquare (T n) {T sq = sqrt(n);  return (sq*sq)==n;}
+string uppercase(string &s) {transform(s.begin(), s.end(), s.begin(), ::toupper); return s;}
+string lowercase(string &s) {transform(s.begin(), s.end(), s.begin(), ::tolower); return s;}
+#define Time cerr << "\nTime Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define thats_what_she_said int main(){ ios::sync_with_stdio(0); cin.tie(nullptr); cout.tie(nullptr);
+template <class T> void MAKE_UNIQUE(T &x){sort(all(x));x.resize(distance(x.begin(),unique(all(x))));}
 template <class T> T ecDst(T x1, T x2, T y1, T y2){ return pow((double)x2-x1,2.0)+(pow((double)y2-y1,2.0)); }
-pair<int,int> DIREC[4] = {{0,-1},{0,1},{1,0},{-1,0}};
-ll myceil(ll a,ll b){return ((a/b)+((a%b)!=0));}
-const ll MOD = 1000000007;
-void donothing(){return;}
 //freopen("output.txt","w",stdout); 
 thats_what_she_said // x minutes 
     //freopen("jumping.in","r",stdin); 
