4e80c64b2b4e31b8762aee09e4efe0ae977ed528
Ahmed Elhady <53586701+01H2A@users.noreply.github.com>
2022-04-29 17:27:38 +0200

    Update monotonic_stack.cpp

--- Library/monotonic_stack.cpp
+++ Library/monotonic_stack.cpp
@@ -24,75 +24,6 @@ vi next_greater(vi v){
     return res;
 }
 
-vi prev_greater(vi v){
-    int n = v.size();
-    vi res(n);
-    stack <int> st;
-    
-    for(int i = 0 ; i < n ;i++){ // reverse iterate the next_greater
-        while(st.size() && v[i] > v[st.top()])
-            st.pop();
-        res[i] = st.empty() ? -1 : v[st.top()];
-        st.push(i);
-    }
-    return res;
-}
-
-vi next_smaller(vi v){
-    int n = v.size();
-    vi res(n);
-    stack <int> st;
-    
-    for(int i= n-1; i >= 0 ; i--){ 
-        while(st.size() && v[i] < v[st.top()])// reverse the condition of next_greater
-            st.pop();
-        res[i] = st.empty() ? -1 : v[st.top()];
-        st.push(i);
-    }
-    return res;
-}
-
-vi prev_smaller(vi v){
-    int n = v.size();
-    vi res(n);
-    stack <int> st;
-
-    for(int i = 0;i < n; i++){// reverse iterate the next_greater
-        while(st.size() && v[i] < v[st.top()])// reverse the contition of next_greater
-            st.pop();
-        res[i] = st.empty() ? -1 : v[st.top()];
-        st.push(i);
-    }
-    return res;
-}
-
-void next_and_pre_greater_one_loop(vi v);
-
-int main(){
-    vector <int> v = {2,3,1,5,4};
-    cout << "next_greater" << endl;
-    print_vec(v); cout << endl;
-    print_vec(next_greater(v));
-    cout <<"\n\n";
-
-    cout << "prev_greater" << endl;
-    print_vec(v); cout << endl;
-    print_vec(prev_greater(v));
-    cout <<"\n\n";
-
-    cout << "next_smaller" << endl;
-    print_vec(v); cout << endl;
-    print_vec(next_smaller(v));
-    cout <<"\n\n";
-
-    cout << "prev_smaller" << endl;
-    print_vec(v); cout << endl;
-    print_vec(prev_smaller(v));
-    cout <<"\n\n";
-
-    next_and_pre_greater_one_loop(v);
-}
-
 void next_and_pre_greater_one_loop(vi v){
     int n = v.size();
     vi prev_greater_vec(n,-1);
@@ -119,6 +50,19 @@ void next_and_pre_greater_one_loop(vi v){
     print_vec(v); cout << endl;
     print_vec(next_greater_vec); cout << endl;
 }
+
+
+int main(){
+    vector <int> v = {2,3,1,5,4};
+    cout << "next_greater" << endl;
+    print_vec(v); cout << endl;
+    print_vec(next_greater(v));
+    cout <<"\n\n";
+
+    next_and_pre_greater_one_loop(v);
+}
+
+
 // https://youtu.be/dtiBmmIPR0E
 
 
@@ -128,18 +72,6 @@ output:
 next_greater
 2 3 1 5 4 
 3 5 5 -1 -1 
-
-prev_greater
-2 3 1 5 4 
--1 -1 3 -1 5 
-
-next_smaller
-2 3 1 5 4 
-1 1 -1 4 -1 
-
-prev_smaller
-2 3 1 5 4 
--1 2 -1 1 1 
 
 prev_greater_vec
 2 3 1 5 4 
